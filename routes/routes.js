2b6d4d1d5f6b83af2e9d2702ac0e6bffdd6fadbd
Rodrigo Alvarenga <raalvarenga423@gmail.com>
2023-08-30 15:43:49 -0700

    Fix file uploading

--- routes/routes.js
+++ routes/routes.js
@@ -4,30 +4,26 @@ const API_KEY = process.env.MONDAY_API_KEY;
 const MONDAY_URL = process.env.MONDAY_URL;
 const fetch = require('node-fetch');
 const controller = require('../controller/file.controller');
+const multer = require('multer');
 
 const router = express.Router();
 
-router.post('/upload-file', controller.upload, (req, res) => {
-  // req.body
+router.post('/upload-file', (req, res) => {
   const url = `${MONDAY_URL}/file`;
 
   const query = 'mutation add_file($file: File!, $itemId: Int!) {add_file_to_column (item_id: $itemId, column_id:"files" file: $file) {id}}';
   const map = req.body.map;
   const variables = req.body.variables;
+  const hasFile = req.files.length > 0;
   console.log(variables);
+  console.log(req.files[0]);
 
-  const image = __basedir + "/resources/temp/" + req.file.originalname;
-  console.log(image);
-  const originalName = req.file.originalname;
+  console.log(hasFile);
 
-  var data = '';
-  const boundary = 'xxxxxxxxxxxxxxx';
-
-  fs.readFile(image, async function(error, content) {
-    if (error) {
-      console.error(error);
-      res.send(500).send(error);
-    }
+  if (hasFile) {
+    const originalName = req.files[0].originalname;
+    var data = '';
+    const boundary = 'xxxxxxxxxxxxxxx';
 
     data += "--" + boundary + "\r\n";
     data += "Content-Disposition: form-data; name=\"query\"; \r\n";
@@ -58,7 +54,8 @@ router.post('/upload-file', controller.upload, (req, res) => {
 
     const payload = Buffer.concat([
       Buffer.from(data, 'utf8'),
-      new Buffer.from(content, 'binary'),
+      // new Buffer.from(req.file, 'binary'),
+      req.files[0].buffer,
       Buffer.from("\r\n--" + boundary + "--\r\n", "utf8"),
     ]);
 
@@ -77,21 +74,33 @@ router.post('/upload-file', controller.upload, (req, res) => {
       .then(json => {
         console.log(json)
         if (json.data && json.data.add_file_to_column && json.data.add_file_to_column.id) {
-          fs.unlink(image, (err) => {
-            if (err) {
-              console.error(err);
-              res.status(500).send(error);
-            }
-          res.status(200).send(json);
-          })
+          return res.status(200).send(json);
         } else {
-          res.status(500).send({
+          return res.status(500).send({
             error: `Couldn\'t upload image ${originalName}`,
           });
         }
       })
       .catch(e => res.status(500).send(e));
-  });
+  } else {
+    return res.status(500).send({
+      error: 'Please uplaod a file',
+    });
+  }
+  // const image = __basedir + "/resources/temp/" + req.file.originalname;
+  // console.log(image);
+  // const originalName = req.file.originalname;
+
+  // fs.readFile(image, async function(error, content) {
+  //   if (error) {
+  //     console.error(error);
+  //     res.send(500).send(error);
+  //   }
+
+  //   console.log('Content', content);
+
+  //   
+  // });
 });
 
 module.exports = router;
